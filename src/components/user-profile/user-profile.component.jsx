8b96a261517b8ef61c7e2a7556e91ff3f99c1951
sridharan.manoharan <sridharan.manoharan@wipro.com>
2019-08-26 22:33:03 +0100

    User post

--- src/components/user-profile/user-profile.component.jsx
+++ src/components/user-profile/user-profile.component.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 
 class UserProfile extends React.Component {
+    state = {posts: []};
+    
+    componentDidMount() {
+        fetch(this.props.dataSource)
+        .then(response => response.json())
+        .then(data => this.setState({posts: data}))
+    }
     render(){
         return(
             <div className='container'>
                 <h1>{this.props.name}</h1>
                 <h2>{this.props.email}</h2>
+                Posts:
+                {
+                    this.state.posts.map(post => (
+                        <div key='post.id' className='post'>
+                            <h1>{post.title}</h1>
+                            <p>{post.body}</p>
+                        </div>
+                    ))
+                }
             </div>
         )
     }
